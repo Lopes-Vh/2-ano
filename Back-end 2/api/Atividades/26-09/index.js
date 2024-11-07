f48aad743e4c4417190cca0de98df6338b88ca99
Darkxps <jvitorlopesalmendra@gmail.com>
2024-11-07 11:21:02 -0400

    first commit

---
+++ Back-end 2/api/Atividades/26-09/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import colecaoFl from './dados/dados.js';
+
+const app = express();
+
+app.get('/aviao', (req, res) => {
+    res.json(colecaoFl);
+});
+
+app.get("/aviao/:idFl", (req, res) => {
+    const idFL = parseInt(req.params.idFl);
+    let mensagemErro = '';
+    let fl;
+
+    if (!isNaN(idFL)) {
+        fl = colecaoFl.find(f => f.id === idFL);
+        if (!fl) {
+            mensagemErro = 'FL não encontrada';
+        }
+    } else {
+        mensagemErro = 'Requisição inválida';
+    }
+
+    if (fl) {
+        res.json(fl);
+    } else {
+        res.status(404).json({ "erro": mensagemErro });
+    }
+});
+
+app.listen(8080, () => {
+    console.log('Servidor iniciado na porta 8080');
+});
