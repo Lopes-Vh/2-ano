8727093b968dd4246d9d15d4a07ae9b73ebe7bf8
Lopes-Vh <jvitorlopesalmendra@gmail.com>
2024-11-08 08:26:20 -0400

    AA

---
+++ Front end 2/React/react-projeto/src/Componentes/Footer/index.js
@@ -0,0 +1,13 @@
+
+export default function header (){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><link href="/sobre">sobre</link></li>
+                    <li><link href="/contanto">contanto</link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
