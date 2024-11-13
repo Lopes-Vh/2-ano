26bc5d1ce4dca7016c781ea3dd609fee49ffd07c
Lopes-Vh <jvitorlopesalmendra@gmail.com>
2024-11-13 08:33:45 -0400

    Paia

---
+++ Front end 2/React/react-projeto/src/app/state/page.js
@@ -0,0 +1,40 @@
+'use client'
+import {useState} from 'react';
+
+export default function State(){
+    // let conteudo= "Bom dia"
+    const[conteudo, setConteudo]= useState('Bom dia')
+    const[name, setName]=useState('')
+
+    const[showDiv, setShowDiv]= useState(true)
+
+    let controlarParagrafo= ()=>{
+        // conteudo= 'Boa tarde'
+        setConteudo('Boa tarde');
+        console.log("conteudo")
+    }
+
+    let controlarInput=(evento)=>{
+       console.log(evento.target.value)
+       setName(evento.target.value)  
+    }
+    return(
+        <div>
+            {/* <p>{conteudo}</p>
+            <button onClick={controlarParagrafo}>mudar</button> */}
+            <button onClick={() => { setShowDiv(!showDiv) }}>
+    {showDiv ? 'Esconder' : 'Mostrar'}
+</button>
+
+            {showDiv &&
+                (
+                    <div> 
+                        <p>Nome: {name}</p>
+                        <input type="text" onChange={controlarInput} />
+                    </div>
+                )
+            }
+            
+        </div>
+    )
+} 
