a1462041d7a17b98cac82dda893fe6a529c82a37
Lopes-Vh <jvitorlopesalmendra@gmail.com>
2024-12-06 08:26:36 -0400

    aaaaaa

--- Front end 2/React/react-projeto/src/app/page.js
+++ Front end 2/React/react-projeto/src/app/page.js
@@ -4,10 +4,10 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function Home() {
-  const nome = 'Lopes'
+  const nome = 'aaa'
   return (
     <div>
-        <h1 className={styles.h1}>Luisito Genio Suarez</h1>
+        <h1 className={styles.h1}>aaaaaaaaaaaaaaaaa</h1>
         <p>Parágrafo da primeira página</p>
         <p>Autor: {nome}</p>
         <Image className= {styles.imagem} src="/img/suarez-2.jpg" alt="Milei" width={500} height={400}/>
