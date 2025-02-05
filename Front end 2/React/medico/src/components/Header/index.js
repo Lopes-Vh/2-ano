dc5731bd892299aa8f05af8e9a5b3198ece65c88
Lopes-Vh <jvitorlopesalmendra@gmail.com>
2025-02-05 08:25:01 -0400

    Byaaaaaaaaaaaaaaaaaaaaaaaaaat

--- Front end 2/React/medico/src/components/Header/index.js
+++ Front end 2/React/medico/src/components/Header/index.js
@@ -7,11 +7,34 @@ import Image from 'next/image';
 
 const Header = () => {
     const [menuVisible, setMenuVisible] = useState(false);
-    const [submenuVisible, setSubmenuVisible] = useState({
-        medico: false,
-        paciente: false,
-        consulta: false,
-    });
+    const [submenuVisible, setSubmenuVisible] = useState({});
+
+    const menuItems = [
+        {
+            key: 'medico',
+            label: 'Médico',
+            links: [
+                { href: '/medico/agenda', label: 'Agenda' },
+                { href: '/medico/perfil', label: 'Perfil' },
+            ],
+        },
+        {
+            key: 'paciente',
+            label: 'Paciente',
+            links: [
+                { href: '/paciente/historico', label: 'Histórico' },
+                { href: '/paciente/dados', label: 'Dados' },
+            ],
+        },
+        {
+            key: 'consulta',
+            label: 'Consulta',
+            links: [
+                { href: '/consulta/solicitar', label: 'Solicitar' },
+                { href: '/consulta/status', label: 'Status' },
+            ],
+        },
+    ];
 
     const toggleSubmenu = (item) => {
         setSubmenuVisible((prev) => ({
@@ -27,7 +50,7 @@ const Header = () => {
                     <h1 className={styles.title}>Header</h1>
                     <Image
                         className={styles.logo}
-                        src="/im g/logo.png"
+                        src="/img/logo.png"
                         alt="Logo"
                         width={50}
                         height={50}
@@ -39,107 +62,36 @@ const Header = () => {
                     aria-label="Toggle menu"
                 >
                     {menuVisible ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="40"
-                            height="40"
-                            fill="currentColor"
-                            className="bi bi-x-lg"
-                            viewBox="0 0 16 16"
-                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                             <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
                         </svg>
                     ) : (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="40"
-                            height="40"
-                            fill="currentColor"
-                            className="bi bi-list"
-                            viewBox="0 0 16 16"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                            />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
                         </svg>
                     )}
                 </button>
             </div>
             <AnimatePresence>
                 {menuVisible && (
-                    <motion.nav
-                        initial={{ opacity: 0, y: -20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: -20 }}
-                    >
+                    <motion.nav initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -20 }}>
                         <ul className={styles.navOptions}>
-                            <li className={styles.navItem} key="medico">
-                                <button onClick={() => toggleSubmenu('medico')}>
-                                    Médico
-                                </button>
-                                <AnimatePresence>
-                                    {submenuVisible.medico && (
-                                        <motion.ul
-                                            initial={{ opacity: 0, y: -10 }}
-                                            animate={{ opacity: 1, y: 0 }}
-                                            exit={{ opacity: 0, y: -10 }}
-                                            className={styles.submenu}
-                                        >
-                                            <li>
-                                                <Link href="/medico/agenda">Agenda</Link>
-                                            </li>
-                                            <li>
-                                                <Link href="/medico/perfil">Perfil</Link>
-                                            </li>
-                                        </motion.ul>
-                                    )}
-                                </AnimatePresence>
-                            </li>
-                            <li className={styles.navItem} key="paciente">
-                                <button onClick={() => toggleSubmenu('paciente')}>
-                                    Paciente
-                                </button>
-                                <AnimatePresence>
-                                    {submenuVisible.paciente && (
-                                        <motion.ul
-                                            initial={{ opacity: 0, y: -10 }}
-                                            animate={{ opacity: 1, y: 0 }}
-                                            exit={{ opacity: 0, y: -10 }}
-                                            className={styles.submenu}
-                                        >
-                                            <li>
-                                                <Link href="/paciente/historico">Histórico</Link>
-                                            </li>
-                                            <li>
-                                                <Link href="/paciente/dados">Dados</Link>
-                                            </li>
-                                        </motion.ul>
-                                    )}
-                                </AnimatePresence>
-                            </li>
-                            <li className={styles.navItem} key="consulta">
-                                <button onClick={() => toggleSubmenu('consulta')}>
-                                    Consulta
-                                </button>
-                                <AnimatePresence>
-                                    {submenuVisible.consulta && (
-                                        <motion.ul
-                                            initial={{ opacity: 0, y: -10 }}
-                                            animate={{ opacity: 1, y: 0 }}
-                                            exit={{ opacity: 0, y: -10 }}
-                                            className={styles.submenu}
-                                        >
-                                            <li>
-                                                <Link href="/consulta/solicitar">Solicitar</Link>
-                                            </li>
-                                            <li>
-                                                <Link href="/consulta/status">Status</Link>
-                                            </li>
-                                        </motion.ul>
-                                    )}
-                                </AnimatePresence>
-                            </li>
+                            {menuItems.map(({ key, label, links }) => (
+                                <li className={styles.navItem} key={key}>
+                                    <button onClick={() => toggleSubmenu(key)}>{label}</button>
+                                    <AnimatePresence>
+                                        {submenuVisible[key] && (
+                                            <motion.ul initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }} className={styles.submenu}>
+                                                {links.map(({ href, label }) => (
+                                                    <li key={href}>
+                                                        <Link href={href}>{label}</Link>
+                                                    </li>
+                                                ))}
+                                            </motion.ul>
+                                        )}
+                                    </AnimatePresence>
+                                </li>
+                            ))}
                         </ul>
                     </motion.nav>
                 )}
