dd2bdf874d0a8656442ac4a4ff3ae31512676f27
Darkxps <jvitorlopesalmendra@gmail.com>
2025-02-18 13:15:17 -0400

    aa

--- Front end 2/React/medico/src/app/layout.js
+++ Front end 2/React/medico/src/app/layout.js
@@ -2,13 +2,13 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 
-export const metadata = {
-  title: "Atividade",
-  charset: 'UTF-8',
-  author: 'Lopes',
-  keywords: 'HTML, CSS, JavaScript, React, Next.js',
-  
-};
+    export const metadata = {
+      title: "Atividade",
+      charset: 'UTF-8',
+      author: 'Lopes',
+      keywords: 'HTML, CSS, JavaScript, React, Next.js',
+      
+    };
 
 export default function RootLayout({ children }) {
   return (
